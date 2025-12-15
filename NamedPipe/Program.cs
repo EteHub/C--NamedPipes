b49057cd4921c2afd792c16ebffd0402818dab5d
ete <andresysfd@gmail.com>
2025-12-15 13:15:48 -0400

    Añadido mensaje de inicio principal

--- NamedPipe/Program.cs
+++ NamedPipe/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.IO.Pipes;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private const string PipeName = "CSharpPipe";
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Servidor de Comandos via Named Pipes ---");
+        Console.WriteLine($"Esperando conexiones en el pipe: '{PipeName}'");
+        Console.WriteLine("Presiona Ctrl+C para salir.");
+    }
+
+}
